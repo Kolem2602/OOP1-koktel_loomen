e5332a548bdba60f8494ba7e60106b7c9a79afb0
ema kolaric <emakolaric@icloud.com>
2019-01-01 22:45:56 +0100

    Add project files.

---
+++ OOP1-koktel_loomen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1_koktel_loomen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
